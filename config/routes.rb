d6a8bd6caaf5c7b560b3a0e3acd354239c144f79
Jason Macauley <jason.macauley@simplybusiness.com>
2019-10-02 13:47:46 -0400

    Trimmed old code

--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,6 @@ Rails.application.routes.draw do
 
   get '/refresh/:id' => 'refresh#refresh', as: :refresh
   get '/refresh' => 'refresh#refresh_all', as: :refresh_all
-  get 'axolotl_stats/index'
-
-  get 'axolotl_stats/show'
-
-  get 'toy/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
