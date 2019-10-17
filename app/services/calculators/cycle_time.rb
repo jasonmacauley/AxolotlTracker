04b093e05c5f016df917df55de5c5073b3aabc95
Jason Macauley <jason.macauley@simplybusiness.com>
2019-10-17 15:07:27 -0400

    Added Cycle time indicator

--- app/services/calculators/cycle_time.rb
+++ app/services/calculators/cycle_time.rb
@@ -1,11 +1,12 @@
 module Calculators
   class CycleTime
+    attr_accessor :lists
     def initialize(board)
       @board = board
       @configs = BoardConfiguration.config_by_board_type(board.id, 'cycle_time_lists')
-      @lists = []
+      self.lists = []
       @configs.each do |config|
-        @lists.push(TrelloList.find_by_trello_id(config.value).name)
+        self.lists.push(TrelloList.find_by_trello_id(config.value).name)
       end
     end
 
@@ -13,7 +14,7 @@ module Calculators
       cycle_time = {}
       history.each do |date, data|
         cycle_time[date] = 0
-        @lists.each do |list|
+        self.lists.each do |list|
           cycle_time[date] += data[list] ? data[list]['average'] : 0
         end
       end
