f3c16be870adf7ae6537b2f902af9f6101fe832e
Jason Macauley <jason.macauley@simplybusiness.com>
2019-10-31 16:06:45 -0400

    added terraform for the db

--- _terraform/postgresql.tf
+++ _terraform/postgresql.tf
@@ -2,32 +2,10 @@
 #  region = "eu-west-1"
 #}
 
-####################################
-# Variables common to both instnaces
-####################################
-locals {
-  engine            = "postgres"
-  engine_version    = "9.6.9"
-  instance_class    = "db.t2.large"
-  allocated_storage = 5
-  port              = "5432"
-}
-
 ##############################################################
 # Data sources to get VPC, subnets and security group details
 ##############################################################
-data "aws_vpc" "default" {
-  default = true
-}
 
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "default"
-}
 
 ###########
 # Master DB
@@ -39,27 +17,65 @@ module "master" {
 
   identifier = "axolotltracker-db-master-postgres"
 
-  engine            = local.engine
-  engine_version    = local.engine_version
-  instance_class    = local.instance_class
-  allocated_storage = local.allocated_storage
+  engine            = "postgres"
+  engine_version    = "9.6.9"
+  instance_class    = "db.t2.large"
+  allocated_storage = 5
 
   name     = "axolotltrackerdbpostgres"
   username = "axolotl"
   password = "!petth3BuringD0g!!"
-  port     = local.port
+  port     = "5432"
 
-  vpc_security_group_ids = [data.aws_security_group.default.id]
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = ["sg-12345678"]
 
   maintenance_window = "Mon:00:00-Mon:03:00"
   backup_window      = "03:00-06:00"
 
-  # Backups are required in order to create a replica
-  backup_retention_period = 1
+  tags = {
+      Owner       = "user"
+      Environment = "dev"
+    }
 
   # DB subnet group
-  subnet_ids = data.aws_subnet_ids.all.ids
+  subnet_ids = ["subnet-e0a87e86", "subnet-fbaa7c9d"]
 
   create_db_option_group    = false
   create_db_parameter_group = false
+
+    # Snapshot name upon DB deletion
+    final_snapshot_identifier = "axolotl-db"
+
+    # Database Deletion Protection
+    deletion_protection = true
+
+    parameters = [
+      {
+        name = "character_set_client"
+        value = "utf8"
+      },
+      {
+        name = "character_set_server"
+        value = "utf8"
+      }
+    ]
+
+    options = [
+      {
+        option_name = "MARIADB_AUDIT_PLUGIN"
+
+        option_settings = [
+          {
+            name  = "SERVER_AUDIT_EVENTS"
+            value = "CONNECT"
+          },
+          {
+            name  = "SERVER_AUDIT_FILE_ROTATIONS"
+            value = "37"
+          },
+        ]
+      },
+    ]
 }
